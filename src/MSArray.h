630da65cbcd0c2124b03634b21918fff7e141a4a
Martin Pokorny <mpokorny@nrao.edu>
2018-01-23 13:24:32 -0700

    Add equality test operators to MSArray

--- src/MSArray.h
+++ src/MSArray.h
@@ -2,7 +2,9 @@
 #ifndef MS_ARRAY_H_
 #define MS_ARRAY_H_
 
+#include <algorithm>
 #include <complex>
+#include <cstring>
 #include <memory>
 #include <vector>
 
@@ -13,9 +15,48 @@ namespace mpims {
 
 struct MSArray {
 
+  MSArray() {
+  }
+
+  MSArray(
+    const std::vector<IndexBlockSequence<MSColumns> >& blocks_,
+    std::shared_ptr<std::complex<float> >& buffer_)
+    : blocks(blocks_)
+    , buffer(buffer_) {
+  }
+
   std::vector<IndexBlockSequence<MSColumns> > blocks;
 
   std::shared_ptr<std::complex<float> > buffer;
+
+  bool
+  operator==(const MSArray& other) const {
+    return (
+      blocks == other.blocks
+      && ((!buffer && !other.buffer)
+          || (buffer && other.buffer
+              && std::memcmp(
+                buffer.get(),
+                other.buffer.get(),
+                num_elements() * sizeof(*buffer)))));
+  }
+
+  bool
+  operator!=(const MSArray& other) const {
+    return !operator==(other);
+  }
+
+  size_t
+  num_elements() const {
+    size_t result = 0;
+    std::for_each(
+      std::begin(blocks),
+      std::end(blocks),
+      [&result](const IndexBlockSequence<MSColumns>& ibs) {
+        result += ibs.num_elements();
+      });
+    return result;
+  }
 
 };
 
