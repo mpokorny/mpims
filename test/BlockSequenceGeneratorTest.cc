503846d6a8c660512084bbc9f5b5fd76c8b36436
Martin Pokorny <mpokorny@nrao.edu>
2018-06-11 15:58:24 -0600

    Add more unit tests for BlockSequenceGenerator

--- test/BlockSequenceGeneratorTest.cc
+++ test/BlockSequenceGeneratorTest.cc
@@ -98,6 +98,78 @@ TEST(BlockSequenceGenerator, RepeatingSequenceBoundedAxis) {
   EXPECT_FALSE(blk);
 }
 
+TEST(BlockSequenceGenerator, TruncatedRepeatingSequence) {
+
+  const std::size_t rep = 15;
+  const std::size_t axreps = 3;
+  const std::vector<block_t> blocks{
+    block_t(0, 2),
+      block_t(5, 3),
+      block_t(12, 1),
+      block_t(rep, 0)};
+  const std::size_t len = axreps * rep - 9;
+
+  auto st = BlockSequenceGenerator::initial_state(blocks, len);
+  std::optional<block_t> blk;
+  for (std::size_t n = 0; n < axreps - 1; ++n)
+    for (std::size_t i = 0; i < blocks.size() - 1; ++i) {
+      std::tie(st, blk) = BlockSequenceGenerator::apply(st);
+      ASSERT_TRUE(blk);
+      block_t rblk =
+        std::make_tuple(
+          std::get<0>(blocks[i]) + n * rep,
+          std::get<1>(blocks[i]));
+      EXPECT_EQ(blk.value(), rblk);
+    }
+  std::tie(st, blk) = BlockSequenceGenerator::apply(st);
+  ASSERT_TRUE(blk);
+  block_t rblk =
+    std::make_tuple(
+      std::get<0>(blocks[0]) + (axreps - 1) * rep,
+      std::get<1>(blocks[0]));
+  EXPECT_EQ(blk.value(), rblk);
+  std::tie(st, blk) = BlockSequenceGenerator::apply(st);
+  ASSERT_TRUE(blk);
+  std::size_t b0 = std::get<0>(blocks[1]) + (axreps - 1) * rep;
+  rblk = std::make_tuple(b0, len - b0);
+  EXPECT_EQ(blk.value(), rblk);
+
+  std::tie(st, blk) = BlockSequenceGenerator::apply(st);
+  EXPECT_FALSE(blk);
+}
+
+TEST(BlockSequenceGenerator, ApproxRepeatingSequenceUnbounded) {
+  const std::size_t rep = 15;
+  const std::vector<block_t> blocks{
+    block_t(0, 2),
+      block_t(5, 3),
+      block_t(12, 1),
+      block_t(rep, 0)};
+
+  auto st = BlockSequenceGenerator::initial_state(blocks, std::nullopt);
+  std::optional<block_t> blk;
+  std::tie(st, blk) = BlockSequenceGenerator::apply(st);
+  ASSERT_TRUE(blk);
+  std::size_t n = 0, bi = 0;
+  while (std::get<0>(blk.value()) < 10000000) {
+
+    block_t rblk =
+      std::make_tuple(
+        std::get<0>(blocks[bi]) + n * rep,
+        std::get<1>(blocks[bi]));
+    ASSERT_EQ(blk.value(), rblk);
+
+    ++bi;
+    if (bi == blocks.size() - 1) {
+      bi = 0;
+      ++n;
+    }
+
+    std::tie(st, blk) = BlockSequenceGenerator::apply(st);
+    ASSERT_TRUE(blk);
+  }
+}
+
 int
 main(int argc, char *argv[]) {
 
